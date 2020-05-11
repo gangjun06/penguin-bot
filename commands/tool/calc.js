af740f3c25247f6c6a1a7a4cfa3bae7fffdd17a9
이강준 <admin@gangjun.dev>
2020-05-11 21:15:14 +0900

    add calc function

--- commands/tool/calc.js
+++ commands/tool/calc.js
@@ -1,10 +1,26 @@
 const { MessageEmbed } = require("discord.js");
+const math = require("mathjs");
 
 module.exports = {
   name: "calc",
   category: "tool",
   description: "calculation expression",
+  usage: "<expression>",
   run: async (client, message, args) => {
-      message.reply("this function is not completed")
+    if (!args[0]) return message.reply("Please input a calculation.");
+    let resp;
+
+    try {
+      resp = math.evaluate(args.join(" "));
+    } catch (e) {
+      return message.reply("Sorry, Please input a valid calculation.");
+    }
+
+    const embed = new MessageEmbed()
+      .setColor("#bedbe9")
+      .setTitle("Math Calculation")
+      .addField("Input", `\`\`\`js\n${args.join(" ")}\`\`\``)
+      .addField("Output", `\`\`\`js\n${resp}\`\`\``);
+    message.channel.send(embed);
   },
 };
