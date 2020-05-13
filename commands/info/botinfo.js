b1653f8b7a6b5ec487bd4c784c799800838c5b2c
noamboy2006 <noamboy2006@gmail.com>
2020-05-13 12:47:09 +0900

    multi-lang support

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -1,12 +1,14 @@
 const { getMember, formatDate } = require("../../utils/util");
 const { MessageEmbed } = require("discord.js");
 const { stripIndent } = require("common-tags");
+const fs = require('fs')
 
 module.exports = {
   name: ["botinfo", "봇정보"],
   category: "info",
   description: ["shows bot info", "봇정보를 보여줍니다"],
   run: async (client, message, args) => {
+    let lang = fs.readdirSync("./language").length
     const embed = new MessageEmbed()
       .setThumbnail(client.user.displayAvatarURL({ format: "png" }))
       .setColor(
@@ -18,7 +20,7 @@ module.exports = {
         "Bot Info",
         stripIndent`**\\> Joined Servers:** ${client.guilds.cache.size}
         **\\> Bot Users:** ${client.users.cache.size}
-        **\\> Available Bot Commands:** ${client.commands.size/2}`,
+        **\\> Available Bot Commands:** ${client.commands.size/lang}`,
         true
       )
     message.channel.send(embed);
